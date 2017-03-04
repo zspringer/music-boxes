069630ab3c074f1c6a6f11adcd968ac59a6c6c5e
zspringer <zspringer.springer@gmail.com>
2017-03-04 14:09:07 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ var boombox = document.getElementById('boombox')
 
 function playTune(num){
   
-  var tuneUrl = url + num + '.mp3'
+  var tuneUrl = url
   console.log(tuneUrl)
+  
+    if(num < 10){
+      tuneUrl += '0'
+    }
+  tuneUrl += num + '.mp3'
   
   boombox.src = tuneUrl
   boombox.play()
